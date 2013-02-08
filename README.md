9b74822ee52ff34ae03756981e7cda5ae563213f
1000000000 <lthyde@gmail.com>
2013-02-08 13:37:24 -0500

    First Commit
    Added README, .gitignore, and some structure for the program

---
+++ README.md
@@ -0,0 +1,20 @@
+#Atomic Words
+
+---
+###Goals:
+The goal is to create a program that accepts words as input and then outputs all the possible ways, if any, to form the words using only atomic symbols (e.g. Hg, As) and then maybe to allow some way for it to save the word as an image file where the atomic symbols are in boxes with their number, mass and full name.
+
+---
+###Objectives:
+####Completed:
+* Readme
+* Basic Structure of main method
+* Help and Quit commands
+
+####Short Term Plans:
+* Enter info about elements
+* Create code to loop through words to break them down into atomic symbols
+
+####Long Term Plans:
+* Allow ion "+" symbol to be used as a "t" when it is allowed (ie when the element has the +1 oxidation state)
+* Allow command to export a word as an image file where each atomic symbol is in a box like the ones on the periodic table of elements
---
+++ src/net/clonecomputers/lab/AtomicWords.java
@@ -0,0 +1,39 @@
+package net.clonecomputers.lab;
+
+public class AtomicWords {
+	
+	private static final String HELP = "\nEnter a word to find out" +
+									   "\n  how it can be represented as" +
+									   "\n  atomic symbols if it can." +
+									   "\nLines started with a non-word" +
+									   "\n  character will be parsed as commands" +
+									   "\nCommands are:" +
+									   "\n  \"help\" to show this dialog" +
+									   "\n  \"quit\", \"exit\", or \"stop\" to exit\n";
+									   
+	
+	public static void main(String[] args) {
+		System.out.println("Atomic Words started");
+		System.out.println(HELP);
+		String input = "";
+		boolean isCommand = false;
+		while(!(isCommand && (input.equalsIgnoreCase("quit") || input.equalsIgnoreCase("exit") || input.equalsIgnoreCase("stop")))) {
+			input = System.console().readLine(">");
+			if(input.isEmpty()) continue;
+			isCommand = !Character.isLetter(input.charAt(0));
+			if(isCommand) input = input.substring(1);
+			input = input.trim();
+			if(input.matches(".*[^A-Za-z].*")) {
+				continue;
+			} else if(isCommand) {
+				if(input.equalsIgnoreCase("help")) {
+					System.out.println(HELP);
+				}
+			} else {
+				System.out.println("Atomicizing word!");
+				//TODO convert word to atomic symbols
+			}
+		}
+		System.out.println("Exiting Atomic Words");
+	}
+}
